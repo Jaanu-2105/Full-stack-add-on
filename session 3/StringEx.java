5e1875d8b922fc48b86566cb8b34d938f5f8c6f8
Jaanu-2105 <jananisiva2105@gmail.com>
2025-03-21 09:31:23 +0530

    Update StringEx.java

--- session 3/StringEx.java
+++ session 3/StringEx.java
@@ -3,8 +3,8 @@ package session3;
 public class StringsEx {
 
 	public static void main(String[] args) {
-		String s1 = "Mugilan"; //String literal
-		String s2 = new String("Moses sir"); //String Object
+		String s1 = "Jaanu"; //String literal
+		String s2 = new String("BCA"); //String Object
 		char[] c1 = {'J', 'A', 'V', 'A'}; //Array of characters
 		String s3 = new String(c1); //char to String
 		
