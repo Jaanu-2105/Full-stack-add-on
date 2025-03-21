7084a4b3fc4d2917722427cc83c5502510939bb1
Jaanu-2105 <jananisiva2105@gmail.com>
2025-03-21 14:38:39 +0530

    Add files via upload

---
+++ Test/oddnumbers.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class Oddnumbers {
+	    public static void main(String[] args) {
+	        
+	        int count = 0;  
+	        int number = 1; 
+	        
+	        while (count < 20) {
+	            System.out.println(number);
+	            number += 2; 
+	            count++;
+	        }
+	    }
+	}
